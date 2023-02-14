c4731bd4f1995006a4f16c43b5b5b83abe1087a1
Mircea Dogaru <mircea.dogaru.md@gmail.com>
2023-02-14 23:45:32 +0000

    Add scrolling bg to pico bird

--- pico_bird.py
+++ pico_bird.py
@@ -17,9 +17,12 @@ _pixel_skip = const(0)
 
 button_x = Button(14)
 
-background = display.create_pen(40, 40, 40)        
+# Pens for drawing in color
+background = display.create_pen(135, 206, 235)        
 white = display.create_pen(255, 255, 255)       
 red = display.create_pen(255, 0, 0)
+dark_green = display.create_pen(34, 139, 34)
+light_green = display.create_pen(124, 252, 0)
 
 bird = [
 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -90,29 +93,60 @@ class Obstacle:
     def render(self):
         display.set_pen(self.pen)
         display.rectangle(self.x - self.thickness, 0, self.thickness, self.y - self.gap)
-        display.rectangle(self.x - self.thickness, self.y + self.gap, self.thickness, height - self.y + self.gap)        
+        display.rectangle(self.x - self.thickness, self.y + self.gap, self.thickness, height - self.y + self.gap)
+        
+class Tree:
+    def __init__(self, x, w, pen):
+        self.x = x
+        self.pen = pen
+        self.width = w
+        self.reset_height()
+    
+    def render(self):
+        display.set_pen(self.pen)
+        display.rectangle(self.x, self.height, self.width, 135 - self.height)
+        
+    def reset_height(self):
+        self.height = random.randint(40, 100)
             
 bird = Bird(40, int(height/2), 16, 16, 0, red, bird)
 obstacles = [
-Obstacle(200, random.randint(15, 120), 20, 20, red),
-Obstacle(280, random.randint(15, 120), 20, 20, red),
-Obstacle(360, random.randint(15, 120), 20, 20, red) 
+    Obstacle(200, random.randint(15, 120), 20, 20, dark_green),
+    Obstacle(280, random.randint(15, 120), 20, 20, dark_green),
+    Obstacle(360, random.randint(15, 120), 20, 20, dark_green) 
 ]
+
+trees = []
+for tree_index in range(0, 9):
+    trees.append(Tree(int(tree_index * 30), 30, light_green))
 
 angle = 0
 _gravity = const(12)
 _t = const(-15)
 _jv = const(-5)
 _obstacle_speed = const(100)
+_background_speed = const(60)
+
+score = 0
 
 @micropython.native
 def update_obstacles():
+    global score
+    
     for obstacle in obstacles:
         obstacle.x -= int(delta * _obstacle_speed)
-        print(obstacle.x)
         if obstacle.x < -20:
             obstacle.x = 260
             obstacle.y = random.randint(15, 120)
+            score += 1
+
+@micropython.native
+def update_trees():
+    for tree in trees:
+        tree.x -= int(delta * _background_speed)
+        if tree.x <= -30:
+            tree.x = 240 + (tree.x + 30)
+            tree.reset_height() 
 
 @micropython.native
 def logic():
@@ -132,13 +166,23 @@ def logic():
     else:
         bird.angle = 360 + bird.velocity * 3
         
+    update_trees()
     update_obstacles()
 
 @micropython.native    
 def render():
-    bird.render()
+    for tree in trees:
+        tree.render()
+    
     for obstacle in obstacles:
         obstacle.render()
+    
+    bird.render()
+        
+    display.set_pen(white)
+    #display.text("FPS: " + str(int(1/delta)), 10, 10, 50, 1)
+    display.text("Score: " + str(score), 140, 2, 100, 2)
+    
     display.update()    
     
 @micropython.native
@@ -151,13 +195,10 @@ def main_loop():
     display.set_pen(background)
     display.clear()
     
-    display.set_pen(white)
-    display.text("FPS: " + str(int(1/delta)), 10, 10, 50, 1)
-    
     logic()
     render()    
     
     #gc.collect()
 
 while True:
-    main_loop()    
+    main_loop()
